13590d99f81124eac7dca04ce9d702e7eb74b0f2
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-11 01:07:02 +0300

    solved some more Informatics problems

---
+++ src/CostOfPurchase.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CostOfPurchase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.println((a * n )+((b*n )/ 100) + " " + (b*n )% 100);
+    }
+}
