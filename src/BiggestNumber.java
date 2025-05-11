88da69202bc3d714db41ce1ff5132e5b8ede30d3
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-12 00:01:11 +0300

    solved more questions

---
+++ src/BiggestNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber =  scanner.nextInt();
+
+
+    }
+}
