5a6c4b361dc4ecb10d77ab2affde9dcc2729c194
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-12 01:04:25 +0300

    solved Knight problem

---
+++ src/Knight.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Knight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row1 = scanner.nextInt();
+        int column1 = scanner.nextInt();
+        int row2 = scanner.nextInt();
+        int column2 = scanner.nextInt();
+
+        if ((Math.abs(row1 - row2) == 2 && Math.abs(column1 - column2) == 1) ||
+                (Math.abs(row1 - row2) == 1 && Math.abs(column1 - column2) == 2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
