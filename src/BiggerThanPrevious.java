d64d7e1e524951444d55343ebff187634634d600
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-13 00:01:36 +0300

    Solved Array problems

---
+++ src/BiggerThanPrevious.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BiggerThanPrevious {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++){
+            array[i] = scanner.nextInt();
+        }
+        int counter = 0;
+        for (int j = 1; j < number; j++){
+            if (array[j] > array[j-1]){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
+    }
+}
