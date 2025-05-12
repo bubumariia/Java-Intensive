cb38410eca510b60beec95dd004e1e3f83405201
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-12 14:12:32 +0300

    added Square problem

---
+++ src/Square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = a; i <= b; i++){
+            double sqrt = Math.sqrt(i);
+            if(sqrt == Math.floor(sqrt)){
+                System.out.println(i);
+            }
+        }
+    }
+}
