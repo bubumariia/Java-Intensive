d304a6a753753aca83710535948aeaa92b8929a4
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-13 00:47:56 +0300

    solved Right Shift problem

---
+++ src/RightShift.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RightShift {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int temp = array[number - 1];
+
+        for (int i = number - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = temp;
+
+        for (int i = 0; i < number; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
