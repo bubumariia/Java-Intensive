586c9d5b5b61dc96616a4d5dd770a198b3f7c4b7
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-18 15:14:41 +0300

    created Calculator file, practiced static and not-static methods

---
+++ src/www/Calculator.java
@@ -0,0 +1,43 @@
+package www;
+
+public class Calculator {
+    //  Non-static method → Needs an object to call
+    public int add(int a, int b){
+        return a+b;
+    }
+
+
+    // Static method → Should be called using the class name - like in Javascript
+    public static int square(int x){
+        return x * x;
+    }
+
+    public static int subtract(int x, int y){
+        return x - y;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int x, int y){
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        // non-static object call
+        Calculator addResult = new Calculator();
+        System.out.println(addResult.add(33,44));
+        // static - no need for object
+        System.out.println(square(5));
+
+        System.out.println(subtract(20, 4));
+
+        Calculator multiplyResult = new Calculator();
+        System.out.println(multiplyResult.multiply(5,8));
+
+        Calculator divideResult = new Calculator();
+        System.out.println(divideResult.divide(30,2));
+
+    }
+}
--- src/www/Main.java
+++ src/www/Main.java
@@ -8,8 +8,19 @@ public class Main {
         // practice while
         int number = 5;
         while (number >= 1){
-            System.out.println(number--);
+            System.out.println(number--); // it prints numbers from 5 to 1
         }
 
+        // practice Array
+        String[] names = {"Nurs", "Mariia", "Pipow"};
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]); // it prints all the names
+        }
+        // multidimensional arrays
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        System.out.println(myNumbers[1][2]); // it prints 7
+
     }
+
+
 }
