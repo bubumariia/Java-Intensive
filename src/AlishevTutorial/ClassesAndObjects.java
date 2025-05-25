8a9d16e56ad550783feb73a74871d4ea6454ce84
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-25 15:55:05 +0300

    practiced methods

--- src/AlishevTutorial/ClassesAndObjects.java
+++ src/AlishevTutorial/ClassesAndObjects.java
@@ -5,15 +5,21 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Mariia";
         person1.age = 28;
-        System.out.println(person1.name);
+        person1.speak();
 
         Person person2 = new Person();
         person2.name = "Nurs";
         person2.age = 34;
-        System.out.println(person2.name);
+        person2.speak();
+
     }
 }
 class Person{
     String name;
     int age;
+
+    void speak(){
+        System.out.println("My name is "+ name+ " and I am " + age + " years old");
+
+    }
 }
