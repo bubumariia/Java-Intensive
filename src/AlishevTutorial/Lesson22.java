fea34d8239dc659880013e4d5aa5437244374bb5
bubumariia <mariiakudaiberdieva@gmail.com>
2025-05-28 00:15:00 +0300

    created lesson22 class and practiced final

---
+++ src/AlishevTutorial/Lesson22.java
@@ -0,0 +1,16 @@
+package AlishevTutorial;
+
+public class Lesson22 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Test{
+    public static final int CONSTANT = 20;
+
+
+//    public void setCONSTANT(int x){
+//        this.CONSTANT = x;
+//    }  You cant do it
+}
